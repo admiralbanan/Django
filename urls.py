f76e1d6b3ada3ba21c9073fd024f76cd5ec99fda
Alexander Kurochkin <110339661+admiralbanan@users.noreply.github.com>
2024-11-09 00:22:21 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,16 @@
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from myapp.views import (
+    PersonListView,
+    CarAccidentListView,
+    CarAccidentCreateView,
+    HomePageView,
+    JokesView
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Подключаем маршруты из myapp
+    path('', HomePageView.as_view(), name='home'),
+    path('persons/', PersonListView.as_view(), name='person_list'),
+    path('accidents/', CarAccidentListView.as_view(), name='car_accident_list'),
+    path('add-accident/', CarAccidentCreateView.as_view(), name='add_car_accident'),
+    path('jokes/', JokesView.as_view(), name='jokes'),  # Новый URL для анекдотов
 ]
