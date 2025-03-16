ba102a15ff203239af7652ce88ee64e45ea7ce15
Alexander Kurochkin <110339661+admiralbanan@users.noreply.github.com>
2025-03-17 01:08:42 +0300

    Add files via upload

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import random
 from datetime import datetime, timedelta
+
+class PersonQuerySet(models.QuerySet):
+    def from_spb(self):
+        return self.filter(city="Санкт-Петербург")
 
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,8 +17,9 @@ class Person(models.Model):
     birth_date = models.DateField()
     city = models.CharField(max_length=50)
 
+    objects = PersonQuerySet.as_manager()
+
     def save(self, *args, **kwargs):
-        # Генерация данных при сохранении, если поля пустые
         if not self.passport_series:
             self.passport_series = f"{random.randint(38, 42)}{random.randint(10, 99)}"
         if not self.passport_number:
@@ -31,5 +38,12 @@ class Person(models.Model):
 
         super().save(*args, **kwargs)
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.city})"
+class PersonLog(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    action = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+@receiver(post_save, sender=Person)
+def log_person_creation(sender, instance, created, **kwargs):
+    if created:
+        PersonLog.objects.create(person=instance, action='Создан новый пользователь')
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,28 @@
-# myapp/views.py
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Person
 
 def person_list(request):
-    persons = Person.objects.all()  # Получаем всех людей из базы данных
+    # Оптимизация запросов
+    persons = Person.objects.all().select_related().only(
+        'id', 'first_name', 'last_name', 'middle_name', 'passport_series',
+        'passport_number', 'birth_date', 'city'
+    )
+
+    # Фильтрация
+    city = request.GET.get('city')
+    if city:
+        persons = persons.filter(city__icontains=city)
+
+    last_name = request.GET.get('last_name')
+    if last_name:
+        persons = persons.filter(last_name__icontains=last_name)
+
+    # Стабильная сортировка для пагинации
+    persons = persons.order_by('id')
+
+    paginator = Paginator(persons, 10)
+    page_number = request.GET.get('page')
+    persons = paginator.get_page(page_number)
+
     return render(request, 'myapp/person_list.html', {'persons': persons})
